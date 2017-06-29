cd11e298441a9c58604e7c94a4d1f550d53b443f
Eric Sauer <esauer@redhat.com>
2017-06-29 16:26:38 -0400

    Initial commit of custom autoscaler

---
+++ custom-autoscaler/README.md
@@ -0,0 +1,63 @@
+# OpenShift Custom Autoscaler Quickstart
+
+The following illustrates how one might use OpenShift's [metrics stack](https://docs.openshift.com/container-platform/latest/install_config/cluster_metrics.html) (Hawkular, Cassandra, Heapster) to develop customized application autoscaling.
+
+## Running & Testing
+
+This script makes a few assumptions about your application
+
+* The pods are managed by a `deploymentConfig`
+* The `deploymentConfig` is named the same as the app. (in this case, `myapp`)
+* The `deploymentConfig` has memory limits set on the containers. i.e.
+  ```
+  apiVersion: v1
+  kind: DeploymentConfig
+  metadata:
+    name: myapp
+  spec:
+    template:
+      spec:
+        container:
+        - resources:
+            limits:
+              memory: 140Mi
+
+  ```
+
+Before running the script, you need to export a few variables into your environment. The following represents a minimal setup. A full description of all variables is below.
+
+```
+export NAMESPACE=myproject
+export TOKEN=`oc whoami -t`
+export APP_NAME=myapp
+export HAWKULAR_HOSTNAME=hawkular-metrics.apps.ocp-c1.myorg.com
+```
+
+From there we can simply run our script.
+
+```
+./scale.py
+```
+
+The script will continue to run until exited, either through Ctrl+C or killing the pid.
+
+## Environment Variables
+
+| Variable Name | Default Value | Description |
+| --------------| ------------- | ----------- |
+| `NAMESPACE` | n/a; Required | Namespace the app is running in. |
+| `TOKEN` | n/a; Required | OpenShift User or ServiceAccount token. Can be retrieved using `oc whoami -t` for users or `oc serviceaccount get-token <sa name>` for serviceaccounts. |
+| `APP_NAME` | n/a; Required | Name of DeploymentConfig. Must match name shown in `oc get dc` |
+| HAWKULAR_HOSTNAME | n/a; Required | Route hostname for hawkular service. i.e. `oc get route hawkular-metrics -n openshift-infra` |
+| METRIC_PULL_INTERVAL_SECONDS | 60 | Number of seconds between each scale up check |
+| SCALE_UP_THRESHOLD | .7 | Floating numeric value 0-1; Percentage of total memory used over which a scale-up will happen |
+| CA_CERT | False | Path to a CA Certificate file to validate Hawkular certificate. Default value of `False` will turn off certificate checks and flash a warning message. |
+
+
+
+## Issues, Limitations & Future Enhancements
+
+- Solution should be deployed as a pod in openshift.
+- Currently only scales up, not down
+- Implementation very specific to memory, but could probably be genericized to pull some other metric.
+- Needs better error handling
---
+++ custom-autoscaler/scale.py
@@ -0,0 +1,153 @@
+#!/bin/python
+
+import requests, os, json, sys, time
+from subprocess import Popen, PIPE
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+
+
+token = os.environ['TOKEN']
+namespace = os.environ['NAMESPACE']
+app_name = os.environ['APP_NAME']
+hawkular_hostname = os.environ['HAWKULAR_HOSTNAME']
+
+if 'METRIC_PULL_INTERVAL_SECONDS' in os.environ:
+    interval = os.environ['METRIC_PULL_INTERVAL_SECONDS']
+else:
+    interval = 60
+
+if 'SCALE_UP_THRESHOLD' in os.environ:
+    scale_up_threshold = os.environ['SCALE_UP_THRESHOLD']
+else:
+    scale_up_threshold = .7
+
+if 'CA_CERT' in os.environ:
+    ca_cert = os.environ['CA_CERT']
+else:
+    print "WARNING: Disabling SSL Verification. This should not be done in Production."
+    print "To get rid of this message, export CA_CERT=/path/to/ca-certificate"
+    requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+    ca_cert = False
+
+metrics_url='https://{}/hawkular/metrics/gauges/data'.format(hawkular_hostname)
+
+def pretty_print_POST(req):
+    """
+    At this point it is completely built and ready
+    to be fired; it is "prepared".
+
+    However pay attention at the formatting used in
+    this function because it is programmed to be pretty
+    printed and may differ from the actual request.
+    """
+    print('{}\n{}\n{}'.format(
+        '-----------START-----------',
+        req.method + ' ' + req.metrics_url + '?' + '&'.join('{}={}'.format(k, v) for k, v in req.params.items()),
+        '\n'.join('{}: {}'.format(k, v) for k, v in req.headers.items()),
+    ))
+
+def get_pods(deployment_config):
+    version = Popen(["oc", "-n", "{}".format(namespace), "get", "dc/{}".format(deployment_config), "-o", "jsonpath={ .status.latestVersion }"], stdout=PIPE).stdout.read()
+
+    pods = Popen(["oc", "-n", "{}".format(namespace), "get", "pods", "-l", "deployment={}-{}".format(deployment_config, version), "-o", "jsonpath={ .items[*].metadata.name }"], stdout=PIPE).stdout.read()
+
+    return pods.split()
+
+def get_app_usage(app_name, pods):
+
+    total_usage = 0
+
+    for pod in pods:
+        total_usage += get_pod_usage(pod, namespace, token)
+
+    return total_usage
+
+def get_app_limit(app_name, pods):
+
+    total_limit = 0
+
+    for pod in pods:
+        total_limit += get_pod_limit(pod, namespace, token)
+
+    return total_limit
+
+def get_pod_usage(pod_name, namespace, token):
+    params = {
+        'tags': 'descriptor_name:memory/usage,pod_name:{}'.format(pod_name),
+        'bucketDuration' : '60000ms',
+        'stacked' : 'true',
+        'start' : '-1mn'
+    }
+
+    headers = {
+        'Content-Type' : 'application/json',
+        'Hawkular-Tenant': namespace,
+        'Authorization' : 'Bearer {}'.format(token)
+    }
+
+    req = requests.Request(
+        'GET',
+        metrics_url,
+        params=params,
+        headers=headers
+        )
+    prepared = req.prepare()
+    s = requests.Session()
+    r = s.send(prepared,
+        verify=ca_cert)
+
+#    pretty_print_POST(req)
+#    print r.text
+
+    usage=r.json()[0]
+    return usage["avg"]
+
+def get_pod_limit(pod_name, namespace, token):
+    params = {
+        'tags': 'descriptor_name:memory/limit,pod_name:{}'.format(pod_name),
+        'bucketDuration' : '60000ms',
+        'stacked' : 'true',
+        'start' : '-1mn'
+    }
+
+    headers = {
+        'Content-Type' : 'application/json',
+        'Hawkular-Tenant': namespace,
+        'Authorization' : 'Bearer {}'.format(token)
+    }
+
+    req = requests.Request(
+        'GET',
+        metrics_url,
+        params=params,
+        headers=headers
+        )
+    prepared = req.prepare()
+    s = requests.Session()
+    r = s.send(prepared,
+        verify=ca_cert)
+
+#    pretty_print_POST(req)
+#    print r.text
+
+    usage=r.json()[0]
+    return usage["avg"]
+
+def scale_up(app_name, pod_count):
+    print Popen(["oc", "scale", "--replicas={}".format(pod_count+1), "dc/{}".format(app_name)], stdout=PIPE).stdout.read()
+
+while True:
+    current_pods = get_pods(app_name)
+
+    usage = get_app_usage(app_name, current_pods)
+    limit = get_app_limit(app_name, current_pods)
+
+    print "Average usage is {}".format(usage)
+    print "Averate limit is {}".format(limit)
+    if usage / limit > scale_up_threshold:
+        print "scaling up!"
+        scale_up(app_name, len(current_pods))
+    else:
+        print "staying put"
+    print ""
+    time.sleep(interval)
