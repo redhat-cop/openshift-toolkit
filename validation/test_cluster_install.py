ca40762f57a33e797908ca9c972af8da77882a1e
Eric Sauer <esauer@redhat.com>
2019-03-20 13:55:55 -0400

    Adding readme, requirements, splitting out master tests

--- validation/test_cluster_install.py
+++ validation/test_cluster_install.py
@@ -12,11 +12,36 @@ def getRunningPodsByLabel(namespace, label):
     return len(ret.items)
 
 
+def getNodeCount():
+    ret = v1.list_node()
+    return len(ret.items)
+
+
 def test_router():
-    assert getRunningPodsByLabel('default',
-                                 'deploymentconfig=router') == 3, "Should have 3 router pods"
+    assert getRunningPodsByLabel(
+        'default', 'deploymentconfig=router') == 3, "Should have 3 router pods"
 
 
 def test_registry():
-    assert getRunningPodsByLabel('default',
-                                 'deploymentconfig=registry') == 3, "Should have 3 registry pods"
+    assert getRunningPodsByLabel(
+        'default', 'deploymentconfig=registry') == 3, "Should have 3 registry pods"
+
+
+def test_etcd():
+    assert getRunningPodsByLabel(
+        'kube-system', 'openshift.io/component=etcd') == 3, "Should have 3 etcd pods"
+
+
+def test_master_api():
+    assert getRunningPodsByLabel(
+        'kube-system', 'openshift.io/component=api') == 3, "Should have 3 master api pods"
+
+
+def test_master_controllers():
+    assert getRunningPodsByLabel(
+        'kube-system', 'openshift.io/component=controllers') == 3, "Should have 3 master controller pods"
+
+
+def test_fluentd():
+    assert getRunningPodsByLabel(
+        'openshift-logging', 'component=fluentd') == getNodeCount()
