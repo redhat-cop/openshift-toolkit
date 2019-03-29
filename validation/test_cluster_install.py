c96f8252dc27e5522f014bb584ef8eda2ce88328
mzali <mzali@redhat.com>
2019-03-29 21:00:22 +0800

    - introduce conftest.py  for pytest config - introduce pytest.ini for CLI autofilled. - PEP8 fix and re-structure based on function for main script. - introduce pytest marker for test suite selection by keyword.
    Signed-off-by: aizuddin85 <mzali@redhat.com>

--- validation/test_cluster_install.py
+++ validation/test_cluster_install.py
@@ -1,6 +1,6 @@
 from kubernetes import client, config
 from jsonpath_ng.ext import parse
-
+import pytest
 
 # Configs can be set in Configuration class directly or using helper utility
 config.load_kube_config()
@@ -9,49 +9,62 @@ v1 = client.CoreV1Api()
 cv1 = client.CustomObjectsApi()
 
 
-def getRouteByName(namespace, name):
+# Generic function #
+def get_route_by_name(namespace, name):
     routes = cv1.list_namespaced_custom_object('route.openshift.io', 'v1',
                                                namespace, 'routes')
     jsonpath_expr = parse('items[?metadata.name == {}].$'.format(name))
     return [match.value for match in jsonpath_expr.find(routes)][0]
 
 
-def getRunningPodsByLabel(namespace, label):
+def get_running_pods_by_label(namespace, label):
     ret = v1.list_namespaced_pod(
         namespace, field_selector='status.phase=Running', label_selector=label)
     return len(ret.items)
 
 
-def getNodeCount():
+def get_node_count():
     ret = v1.list_node()
     return len(ret.items)
 
 
-def test_router():
-    assert getRunningPodsByLabel(
-        'default', 'deploymentconfig=router') == 3, "Should have 3 router pods"
+# Master Test Section #
+@pytest.mark.master
+def test_master_controllers(master_node_count):
+    assert get_running_pods_by_label(
+        'kube-system', 'openshift.io/component=controllers') == int(master_node_count), \
+        "Should have {} master controller pods".format(master_node_count)
+
+@pytest.mark.master
+def test_master_api(master_node_count):
+    assert get_running_pods_by_label(
+        'kube-system', 'openshift.io/component=api') == int(master_node_count), \
+        "Should have {} master api pods".format(master_node_count)
+
+@pytest.mark.master
+def test_etcd(etcd_node_count):
+    assert get_running_pods_by_label(
+        'kube-system', 'openshift.io/component=etcd') == int(etcd_node_count), \
+        "Should have {} etcd pods".format(etcd_node_count)
 
 
-def test_registry():
-    assert getRunningPodsByLabel(
-        'default', 'deploymentconfig=registry') == 3, "Should have 3 registry pods"
+# Infra Test Section #
+@pytest.mark.infra
+def test_router(router_node_count):
+    assert get_running_pods_by_label(
+        'default', 'deploymentconfig=router') == int(router_node_count), \
+        "Should have {} router pods".format(router_node_count)
+
+@pytest.mark.infra
+def test_registry(registry_pod_count):
+    assert get_running_pods_by_label(
+        'default', 'deploymentconfig=docker-registry') == int(registry_pod_count), \
+        "Should have {} registry pods".format(registry_pod_count)
 
 
-def test_etcd():
-    assert getRunningPodsByLabel(
-        'kube-system', 'openshift.io/component=etcd') == 3, "Should have 3 etcd pods"
-
-
-def test_master_api():
-    assert getRunningPodsByLabel(
-        'kube-system', 'openshift.io/component=api') == 3, "Should have 3 master api pods"
-
-
-def test_master_controllers():
-    assert getRunningPodsByLabel(
-        'kube-system', 'openshift.io/component=controllers') == 3, "Should have 3 master controller pods"
-
-
+# Logging Test Section #
+@pytest.mark.logging
 def test_fluentd():
-    assert getRunningPodsByLabel(
-        'openshift-logging', 'component=fluentd') == getNodeCount(), "Should have one fluentd pod for every node in the cluster"
+    assert get_running_pods_by_label(
+        'openshift-logging', 'component=fluentd') == get_node_count(), \
+        "Should have one fluentd pod for every node in the cluster"
