b43de03aed24325819ee67242a8d5e32c166b248
afcollins <afcollins@users.noreply.github.com>
2019-03-30 10:16:33 -0700

    Fix etcd backup for OCP 3.11 (#75)

--- backup_restore/ocp-etcd3-pods-backup.sh
+++ backup_restore/ocp-etcd3-pods-backup.sh
@@ -100,14 +100,14 @@ backup_data() {
   # snapshot output is /var/lib/etcd/ because is mounted from the host, and we can move it later to another host folder.
   # > /usr/local/bin/master-exec etcd etcd /bin/bash -c "ETCDCTL_API=3 /usr/bin/etcdctl \
   # --cert /etc/etcd/peer.crt --key /etc/etcd/peer.key --cacert /etc/etcd/ca.crt --endpoints ${ETCD_EP} snapshot save /var/lib/etcd/snapshot.db"
-  ${MASTER_EXEC} etcd etcd /bin/bash -c "ETCDCTL_API=3 /usr/bin/etcdctl \
+  ${MASTER_EXEC} etcd etcd /bin/sh -c "ETCDCTL_API=3 etcdctl \
   --cert /etc/etcd/peer.crt --key /etc/etcd/peer.key --cacert /etc/etcd/ca.crt \
   --endpoints ${ETCD_EP} snapshot save /var/lib/etcd/snapshot.db"
 
   log "Backing up ETCD data, validating snapshot."
   # Validate the status of the snapshot
   # > snapshot status /var/lib/etcd/snapshot.db 
-  ${MASTER_EXEC} etcd etcd /bin/bash -c "ETCDCTL_API=3 /usr/bin/etcdctl \
+  ${MASTER_EXEC} etcd etcd /bin/sh -c "ETCDCTL_API=3 etcdctl \
   --cert /etc/etcd/peer.crt --key /etc/etcd/peer.key --cacert /etc/etcd/ca.crt \
   --endpoints ${ETCD_EP} snapshot status /var/lib/etcd/snapshot.db"
 
